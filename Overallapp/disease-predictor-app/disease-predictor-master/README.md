c52f5810a1a8ce3f474785d3c39754201f7870cc
Ananya Ghosh <75447439+A-GHOSH-dev@users.noreply.github.com>
2023-04-15 00:09:11 +0530

    Delete Overallapp/disease-predictor-app/disease-predictor-master directory

--- Overallapp/disease-predictor-app/disease-predictor-master/README.md
+++
@@ -1,6 +0,0 @@
-# Disease Predictor
-
-A web app for heart disease prediction, diabetes prediction and breast cancer prediciton using Machine Learning based on the Kaggle Datasets. 
-
-
-
--- Overallapp/disease-predictor-app/disease-predictor-master/predictor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('heart', views.heart, name="heart"),
-    path('diabetes', views.diabetes, name="diabetes"),
-    path('breast', views.breast, name="breast"),
-    path('', views.home, name="home"),
-]
